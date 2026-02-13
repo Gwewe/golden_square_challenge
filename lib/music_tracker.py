4d3132dd7c02648ce0c57cefd31af20e58490193
Wedna Guirand <wedna.guirand@gmail.com>
2026-02-13 16:33:24 +0000

    Added the remaining tests and codes to implement full behaviour

--- lib/music_tracker.py
+++ lib/music_tracker.py
@@ -4,7 +4,10 @@ class MusicTracker:
         self._music_db = []
 
     def add_music_track(self, track):
+        if not isinstance(track, str):
+            raise TypeError("The track must be a string")
         return self._music_db.append(track)
+
 
     def get_music_list(self):
         return  self._music_db
--- tests/test_music_tracker.py
+++ tests/test_music_tracker.py
@@ -1,4 +1,5 @@
 from lib.music_tracker import MusicTracker
+import pytest
 
 """
 Initally, the function returns an empty list
@@ -22,30 +23,39 @@ def test_one_track_added_returns_the_track():
 Given two tracks added to the list,
 the function returns the list with the two tracks
 """
-music_tracker = MusicTracker()
-music_tracker.add_music_track("Artist: Jacob Banks, Track: Part time lover")
-music_tracker.add_music_track("Artist: Hot face, Track: Bumble Been")
+def test_two_track_added_returns_both_tracks():
+    music_tracker = MusicTracker()
+    
+    music_tracker.add_music_track("Artist: Jacob Banks, Track: Part time lover")
+    music_tracker.add_music_track("Artist: Hot face, Track: Bumble Been")
 
-assert music_tracker.get_music_list() == ["Artist: Jacob Banks, Track: Part time lover", "Artist: Hot face, Track: Bumble Been"]
+    assert music_tracker.get_music_list() == ["Artist: Jacob Banks, Track: Part time lover", "Artist: Hot face, Track: Bumble Been"]
+
+
 
 """
 Given three tracks added to the list,
 the function returns the list with the three tracks
 """
-music_tracker = MusicTracker()
+def test_three_track_added_returns_all_three_tracks():
 
-music_tracker.add_music_track("Artist: Golden days, Track: Whiteland")
-music_tracker.add_music_track("Artist: Jacob Banks, Track: Part time lover")
-music_tracker.add_music_track("Artist: Hot face, Track: Bumble Been")
+    music_tracker = MusicTracker()
 
-music_tracker.get_music_list() # => ["Artist: Golden days, Track: Whiteland", "Artist: Jacob Banks, Track: Part time lover", "Artist: Hot face, Track: Bumble Been"]
+    music_tracker.add_music_track("Artist: Golden days, Track: Whiteland")
+    music_tracker.add_music_track("Artist: Jacob Banks, Track: Part time lover")
+    music_tracker.add_music_track("Artist: Hot face, Track: Bumble Been")
+
+    assert music_tracker.get_music_list() == ["Artist: Golden days, Track: Whiteland", "Artist: Jacob Banks, Track: Part time lover", "Artist: Hot face, Track: Bumble Been"]
 
 
 """
 When the track is not string
 Throws an exception "The track must be a string"
 """
-music_tracker = MusicTracker()
+def test_add_non_valid_track_throws_error():
 
-music_tracker.add_music_track(1)
-# => Throws an exception "The track must be a string"
+    music_tracker = MusicTracker()
+    with pytest.raises(TypeError) as error:
+        music_tracker.add_music_track(1)
+    
+    assert str(error.value) == "The track must be a string"
